b9a1239e258aab21f0e8ef632fa926bf4b248f4f
Pavel Semenov <PNSemenov@sberbank.ru>
2022-02-02 08:53:18 +0300

    added users micro and aop utils

---
+++ planner-utils/src/main/java/ru/javabegin/micro/planner/utils/aop/LoggingAspect.java
@@ -0,0 +1,39 @@
+package ru.javabegin.micro.planner.utils.aop;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Log
+public class LoggingAspect {
+
+    //аспект будет выполняться для всех методов из пакета контроллеров
+    @Around("execution(* ru.javabegin.micro.planner.todo.controller..*(..)))")
+    public Object profileControllerMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        // получить информацию о том, какой класс и метод выполняется
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.info("-------- Executing " + className + "." + methodName + "   ----------- ");
+
+        StopWatch countDown = new StopWatch();
+
+        //засекаем время
+        countDown.start();
+        Object result = proceedingJoinPoint.proceed();
+        countDown.stop();
+
+        log.info("-------- Execution time of  " + className + "." + methodName + " :: " + countDown.getTotalTimeMillis() + "ms  ----------- ");
+
+        return result;
+    }
+
+}
